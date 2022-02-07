e402e24c2acfe2c64cbae5fb2d3a71546cd846c6
Adam <81320110+adamplogan@users.noreply.github.com>
2022-02-07 17:18:16 -0500

    Reverted the Bishop and Rook to previous movement logic
    My bishop and rook implementation is commented out above if you would like to use that instead.

--- Assets/_Scripts/Game/Pieces/Bishop.cs
+++ Assets/_Scripts/Game/Pieces/Bishop.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class Bishop : Piece
 {
+    Vector2Int[] directions = new Vector2Int[]
+    {
+        new Vector2Int(-1, 1),
+        new Vector2Int(0, 1),
+        new Vector2Int(1, 1),
+        new Vector2Int(-1, 0),
+        new Vector2Int(1, 0),
+        new Vector2Int(-1, -1),
+        new Vector2Int(0, -1),
+        new Vector2Int(1, -1),
+    };
+
     public override List<Vector2Int> FindAvailableSquares()
     {
         AvailableMoves.Clear();
         int reachDistance = 2;
-        int[,] radiusMatrix = CreatePieceRadiusMatrix(occupiedSquare, reachDistance);
-        AvailableMoves.AddRange(GetSquaresInRange(radiusMatrix, occupiedSquare));
-        AvailableMoves.AddRange(GetAdjacentEnemySquares(occupiedSquare));
+
+        // The code that is commented below allows the bishop to move 3 squares WITHOUT having to move in a straight line
+        // **If you want to use this code you must comment out the foreach loop below it**
+
+        //int[,] radiusMatrix = CreatePieceRadiusMatrix(occupiedSquare, reachDistance);
+        //AvailableMoves.AddRange(GetSquaresInRange(radiusMatrix, occupiedSquare));
+        //AvailableMoves.AddRange(GetAdjacentEnemySquares(occupiedSquare));
+
+
+        // The code below allows the bishop to move 3 squares WITH having to move in a straight line
+        foreach (var direction in directions)
+        {
+            for (int i = 1; i <= reachDistance; i++)
+            {
+                Vector2Int nextCoords = occupiedSquare + direction * i;
+                Piece piece = board.GetPieceOnSquare(nextCoords);
+                if (!board.CheckIfCoordsAreOnBoard(nextCoords))
+                    break;
+                if (piece == null)
+                    TryToAddMove(nextCoords);
+                else if (!piece.IsFromSameTeam(this) && i == 1) //Capturing, must be adjacent
+                {
+                    TryToAddMove(nextCoords);
+                    break;
+                }
+                else if (piece.IsFromSameTeam(this))
+                    break;
+            }
+        }
 
         return AvailableMoves;
     }
--- Assets/_Scripts/Game/Pieces/Rook.cs
+++ Assets/_Scripts/Game/Pieces/Rook.cs
@@ -4,12 +4,43 @@ using UnityEngine;
 
 public class Rook : Piece
 {
+    Vector2Int[] directions = new Vector2Int[]
+    {
+        new Vector2Int(-1, 1),
+        new Vector2Int(0, 1),
+        new Vector2Int(1, 1),
+        new Vector2Int(-1, 0),
+        new Vector2Int(1, 0),
+        new Vector2Int(-1, -1),
+        new Vector2Int(0, -1),
+        new Vector2Int(1, -1),
+    };
     public override List<Vector2Int> FindAvailableSquares()
     {
         AvailableMoves.Clear();
         int reachDistance = 2;
-        int[,] radiusMatrix = CreatePieceRadiusMatrix(occupiedSquare, reachDistance);
-        AvailableMoves.AddRange(GetSquaresInRange(radiusMatrix, occupiedSquare));
+
+        // The code that is commented below allows the ROOK to move 2 squares WITHOUT having to move in a straight line
+        // **If you want to use this code you must comment out the foreach loop below it**
+
+        //int[,] radiusMatrix = CreatePieceRadiusMatrix(occupiedSquare, reachDistance);
+        //AvailableMoves.AddRange(GetSquaresInRange(radiusMatrix, occupiedSquare));
+
+        // The code below allows the ROOK to move 2 squares WITH having to move in a straight line
+        foreach (var direction in directions)
+        {
+            for (int i = 1; i <= reachDistance; i++)
+            {
+                Vector2Int nextCoords = occupiedSquare + direction * i;
+                Piece piece = board.GetPieceOnSquare(nextCoords);
+                if (!board.CheckIfCoordsAreOnBoard(nextCoords))
+                    break;
+                if (piece == null)
+                    TryToAddMove(nextCoords);
+                else
+                    break;
+            }
+        }
 
         //Capturing, can shoot up to two squares in any direction over other units. Two squares does not count Rook or enemy.
         for (int x = -3; x <= 3; x++)
