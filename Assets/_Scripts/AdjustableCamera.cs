c9f32179483336d765fa896f3a2657e40949a11e
Adam <81320110+adamplogan@users.noreply.github.com>
2022-02-13 12:46:03 -0500

    360 Camera Modifications
    The cam can lock into place for all 4 sides of the chess board, overhead view, and angled view.

--- Assets/_Scripts/AdjustableCamera.cs
+++ Assets/_Scripts/AdjustableCamera.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class AdjustableCamera : MonoBehaviour
 {
     [SerializeField] private Camera mainCam;
-    private const float Y_ANGLE_MIN = 10.0f;
-    private const float Y_ANGLE_MAX = 70.0f;
+    private const float Y_ANGLE_MIN = 23.0f;
+    private const float Y_ANGLE_MAX = 89.0f;
 
     public Transform lookAt;
     public Transform camTransform;
@@ -15,11 +15,13 @@ public class AdjustableCamera : MonoBehaviour
 
     private float distance = 0.60f;
     private float currentX = 0.0f;
-    private float currentY = 32.0f;
-    private float sensitivityX = 3.0f;
-    private float sensitivityY = 2.0f;
+    private float currentY = 40.0f;
+    private float sensitivityX = 2.9f;
+    private float sensitivityY = 2.3f;
     private float scrollSensitivity = 3.0f;
-
+    private float lockStrengthX = 2.5f;
+    private float lockStrengthY = 1.5f;
+    private float lockStrengthEdgeY = 1.1f;
     private void Start()
     {
         camTransform = transform;
@@ -29,16 +31,36 @@ public class AdjustableCamera : MonoBehaviour
     private void Update()
     {
         if (Input.GetMouseButton(0))
-        {
+        { 
             currentX += Input.GetAxis("Mouse X")*sensitivityX;
             currentY += -Input.GetAxis("Mouse Y")*sensitivityY;
+
+            //Lock X if current X is in range of these values
+            if (currentX < lockStrengthX && currentX > -lockStrengthX)
+                currentX = 0;
+            else if (currentX < 90 + lockStrengthX && currentX > 90 - lockStrengthX)
+                currentX = 90;
+            else if (currentX < 180 + lockStrengthX && currentX > 180 - lockStrengthX)
+                currentX = 180;
+            else if (currentX > -180 - lockStrengthX && currentX < -180 + lockStrengthX)
+                currentX = 180;
+            else if (currentX < -90 + lockStrengthX && currentX > -90 - lockStrengthX)
+                currentX = -90;
+            else if (currentX < 270 + lockStrengthX && currentX > 270 - lockStrengthX)
+                currentX = -90;
+
+            //Lock Y if current Y is in range of these values
+            if (currentY < 40 + lockStrengthY && currentY > 40 - lockStrengthY)
+                currentY = 40;
+            else if (currentY < 89 + lockStrengthEdgeY && currentY > 89 - lockStrengthEdgeY)
+                currentY = 89;
             currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
         }
         if (Input.GetAxis ("Mouse ScrollWheel") > 0 && GetComponent<Camera>().fieldOfView > 36)
         {
             GetComponent<Camera>().fieldOfView -= scrollSensitivity;
         }
-        if (Input.GetAxis("Mouse ScrollWheel") < 0 && GetComponent<Camera>().fieldOfView < 55)
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && GetComponent<Camera>().fieldOfView < 60)
         {
             GetComponent<Camera>().fieldOfView += scrollSensitivity;
         }
