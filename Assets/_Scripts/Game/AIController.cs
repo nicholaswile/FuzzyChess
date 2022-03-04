8b0a69efa943a5d01c0141b8ffbe24cef15728a9
Adam <81320110+adamplogan@users.noreply.github.com>
2022-03-03 20:05:55 -0500

    AI can take a full turn

--- Assets/_Scripts/Game/AIController.cs
+++ Assets/_Scripts/Game/AIController.cs
@@ -7,31 +7,40 @@ public class AIController : MonoBehaviour
 {
     [SerializeField] private ChessBoard board;
     [SerializeField] private GameController controller;
+    [SerializeField] private GameUI gameUI;
 
     private IEnumerator AI_TakeTurn_Coroutine()
     {
-        foreach (Piece corpPiece in controller.blackPlayer.LeftCorpPieces.ToList())
+        List<Piece> activeCorpPieces = controller.blackPlayer.KingCorpPieces;
+        while (controller.activePlayer == controller.blackPlayer)
         {
-            Vector3 piecePosition = board.GetPositionFromCoords(corpPiece.occupiedSquare);
-
-            if (board.isSelectable(corpPiece) && corpPiece.AvailableMoves.Count > 0)
+            foreach (Piece corpPiece in activeCorpPieces.ToList())
             {
-                yield return new WaitForSeconds(1);
-                board.OnSquareSelected(piecePosition);
+                Vector3 piecePosition = board.GetPositionFromCoords(corpPiece.occupiedSquare);
 
-
-                foreach (Vector2Int move in corpPiece.AvailableMoves.ToList())
+                if (board.isSelectable(corpPiece) && corpPiece.AvailableMoves.Count > 0)
                 {
-                    Vector3 movePosition = board.GetPositionFromCoords(move);
-
                     yield return new WaitForSeconds(1);
-                    board.OnSquareSelected(movePosition);
+                    board.OnSquareSelected(piecePosition);
 
+                    foreach (Vector2Int move in corpPiece.AvailableMoves.ToList())
+                    {
+                        Vector3 movePosition = board.GetPositionFromCoords(move);
+
+                        yield return new WaitForSeconds(1);
+                        board.OnSquareSelected(movePosition);
+
+                        break;
+                    }
                     break;
                 }
-                break;
-
             }
+            if (activeCorpPieces == controller.blackPlayer.KingCorpPieces) 
+                activeCorpPieces = controller.blackPlayer.RightCorpPieces;
+            else if (activeCorpPieces == controller.blackPlayer.RightCorpPieces) 
+                activeCorpPieces = controller.blackPlayer.LeftCorpPieces;
+            else if (activeCorpPieces == controller.blackPlayer.LeftCorpPieces) 
+                activeCorpPieces = controller.blackPlayer.KingCorpPieces;
         }
     }
     public void AI_TakeTurn() 
