7e4aba446c27008d8d362e7d6b5b616babd5917d
tylerw7391 <56974220+tylerw7391@users.noreply.github.com>
2022-03-09 13:02:56 -0500

    Preliminary work
    Implemented naive algorithm that is semi-functional at determining which spots are in danger of being attacked by the enemy on their next turn.

--- Assets/_Scripts/Game/AIController.cs
+++ Assets/_Scripts/Game/AIController.cs
@@ -12,19 +12,74 @@ public class AIController : MonoBehaviour
     private IEnumerator AI_TakeTurn_Coroutine()
     {
         List<Piece> activeCorpPieces = controller.blackPlayer.KingCorpPieces;
+        List<Piece> enemyPieces = controller.whitePlayer.ActivePieces;
+        List<Vector2Int> potentialCaptures = new List<Vector2Int>();
+
+        //scan each enemy piece and determine what pieces that said piece could capture immediately.
+        //status: naive to pieces that can move more than once per turn, and knights. functional otherwise.
+        //(example: a bishop can move more than once during a turn. It can move, place itself, then capture from its new position.)
+        //This means that for each piece that is capable of moving more than once in a turn, it must not only be assessed
+        //for its potential to capture in its first position, but also its potential to capture in every other position it can reach.
+        foreach (Piece corpPiece in enemyPieces.ToList())
+        {
+            //Debug.Log("Reached inside of enemypieces");
+            //Debug.Log("Number of enemy pieces: " + enemyPieces.Count);
+            //Debug.Log("Available moves: " + corpPiece.AvailableMoves.Count);
+            Vector3 piecePosition = board.GetPositionFromCoords(corpPiece.occupiedSquare);
+
+            if (corpPiece.AvailableMoves.Count > 0)
+            {
+                //Debug.Log("Reached inside of isselectable");
+                //Debug.Log(corpPiece.AvailableMoves.Count);
+                //yield return new WaitForSeconds(1);
+                //board.OnSquareSelected(piecePosition);
+
+                foreach (Vector2Int move in corpPiece.AvailableMoves.ToList())
+                {
+                    //see if we can detect that a "take" is available.
+                    //Debug.Log("Reached inside of availablemoves");
+                    Debug.Log(move);
+                    Piece piece = board.GetPieceOnSquare(move);
+                    if (piece != null && !corpPiece.IsFromSameTeam(piece))
+                    {
+                        Debug.Log("Found cap");
+                        potentialCaptures.Add(move);
+                    }
+
+                }
+            }
+        }
+
+        foreach(Vector2Int potcap in potentialCaptures.ToList())
+        {
+            Debug.Log(potcap);
+        }
+        Debug.Log("There are " + potentialCaptures.Count + " captures that could occur.");
+        potentialCaptures.Clear();
+
         while (controller.activePlayer == controller.blackPlayer)
         {
-            foreach (Piece corpPiece in activeCorpPieces.ToList())
+                foreach (Piece corpPiece in activeCorpPieces.ToList())
             {
                 Vector3 piecePosition = board.GetPositionFromCoords(corpPiece.occupiedSquare);
 
                 if (board.isSelectable(corpPiece) && corpPiece.AvailableMoves.Count > 0)
                 {
+                    //Debug.Log(corpPiece.AvailableMoves.Count);
                     yield return new WaitForSeconds(1);
                     board.OnSquareSelected(piecePosition);
 
                     foreach (Vector2Int move in corpPiece.AvailableMoves.ToList())
                     {
+                        //see if we can detect that a "take" is available.
+                        //Debug.Log(move);
+                        //Piece piece = board.GetPieceOnSquare(move);
+                        //if (piece != null && !corpPiece.IsFromSameTeam(piece)) Debug.Log("Found cap");
+                    }
+
+                        foreach (Vector2Int move in corpPiece.AvailableMoves.ToList())
+                    {
+                        //Debug.Log(move);
                         Vector3 movePosition = board.GetPositionFromCoords(move);
 
                         yield return new WaitForSeconds(1);
