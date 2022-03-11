c489bc612a63e85eb5d4788f77a891d93e91b999
tylerw7391 <56974220+tylerw7391@users.noreply.github.com>
2022-03-11 14:31:46 -0500

    Capture detection
    Implemented a semi-modular system which can detect and store the possible capture options of whichever team's pieces have been given to the system.

--- Assets/_Scripts/Game/AIController.cs
+++ Assets/_Scripts/Game/AIController.cs
@@ -8,54 +8,32 @@ public class AIController : MonoBehaviour
     [SerializeField] private ChessBoard board;
     [SerializeField] private GameController controller;
     [SerializeField] private GameUI gameUI;
+    private List<Vector2Int> potentialCaptures = new List<Vector2Int>();
+
+    //capturePairs<coordinates of piece in danger of being captured, list of coordinates of pieces that may capture the key piece> 
+    private Dictionary<Vector2Int, List<Vector2Int>> capturePairs = new Dictionary<Vector2Int,List<Vector2Int>>();
 
     private IEnumerator AI_TakeTurn_Coroutine()
     {
         List<Piece> activeCorpPieces = controller.blackPlayer.KingCorpPieces;
         List<Piece> enemyPieces = controller.whitePlayer.ActivePieces;
-        List<Vector2Int> potentialCaptures = new List<Vector2Int>();
 
-        //scan each enemy piece and determine what pieces that said piece could capture immediately.
-        //status: naive to pieces that can move more than once per turn, and knights. functional otherwise.
-        //(example: a bishop can move more than once during a turn. It can move, place itself, then capture from its new position.)
-        //This means that for each piece that is capable of moving more than once in a turn, it must not only be assessed
-        //for its potential to capture in its first position, but also its potential to capture in every other position it can reach.
-        foreach (Piece corpPiece in enemyPieces.ToList())
-        {
-            //Debug.Log("Reached inside of enemypieces");
-            //Debug.Log("Number of enemy pieces: " + enemyPieces.Count);
-            //Debug.Log("Available moves: " + corpPiece.AvailableMoves.Count);
-            Vector3 piecePosition = board.GetPositionFromCoords(corpPiece.occupiedSquare);
-
-            if (corpPiece.AvailableMoves.Count > 0)
-            {
-                //Debug.Log("Reached inside of isselectable");
-                //Debug.Log(corpPiece.AvailableMoves.Count);
-                //yield return new WaitForSeconds(1);
-                //board.OnSquareSelected(piecePosition);
-
-                foreach (Vector2Int move in corpPiece.AvailableMoves.ToList())
-                {
-                    //see if we can detect that a "take" is available.
-                    //Debug.Log("Reached inside of availablemoves");
-                    Debug.Log(move);
-                    Piece piece = board.GetPieceOnSquare(move);
-                    if (piece != null && !corpPiece.IsFromSameTeam(piece))
-                    {
-                        Debug.Log("Found cap");
-                        potentialCaptures.Add(move);
-                    }
-
-                }
-            }
-        }
+        //use method PotentialCaptureFinder to determine which enemy pieces are able to capture friendly pieces
+        PotentialCaptureFinder(enemyPieces);
 
         foreach(Vector2Int potcap in potentialCaptures.ToList())
         {
-            Debug.Log(potcap);
+            //Debug.Log(potcap + " Name of piece " + board.GetPieceOnSquare(potcap).pieceType);
+            if(capturePairs.ContainsKey(potcap))
+            {
+                foreach (Vector2Int contCoords in capturePairs[potcap].ToList())
+                {
+                    Debug.Log("Friendly " + board.GetPieceOnSquare(potcap).pieceType + " at " + potcap + 
+                        " contested by : " + board.GetPieceOnSquare(contCoords).pieceType + " at " + contCoords);
+                }
+            }
         }
-        Debug.Log("There are " + potentialCaptures.Count + " captures that could occur.");
-        potentialCaptures.Clear();
+        Debug.Log("There are " + potentialCaptures.Count + " pieces that could be captured this turn.");
 
         while (controller.activePlayer == controller.blackPlayer)
         {
@@ -98,6 +76,121 @@ public class AIController : MonoBehaviour
                 activeCorpPieces = controller.blackPlayer.KingCorpPieces;
         }
     }
+
+    //Creates a list of type Vector2Int which stores all the locations of pieces which a knight may roll a +1 on capture.
+    //This was created because knight movement in AvailableMoves doesn't list places a knight may move to, then capture around.
+    //Pieces stored in this list are at a heightened risk of capture, as knights get +1 on their roll for these.
+    private List<Vector2Int> KnightHighDangerZone(Piece piece)
+    {
+        List<Vector2Int> dangerSpots = new List<Vector2Int>();
+        foreach (Vector2Int move in piece.AvailableMoves.ToList())
+        {
+            Debug.Log("Knight first move location: " + move);
+            //gather a list of new knight moves at each location that the knight could potentially land.
+            Piece contestedPiece = board.GetPieceOnSquare(move);
+
+            //check to make sure we're not looking at already-occupied squares. knight can't move after killing.
+            if (contestedPiece != null && !piece.IsFromSameTeam(contestedPiece))
+            {
+                continue;
+            }
+            else
+            {
+                List<Vector2Int> newKnightMoves = piece.GetAdjacentEnemySquares(move);
+                foreach (Vector2Int futureSpot in newKnightMoves)
+                {
+                    //unneeded, since GetAdjacentEnemySquares already checks if there's a piece and if it's an enemy on the square.
+                    //Piece possiblePiece = board.GetPieceOnSquare(futureSpot);
+
+                    //go through the usual checks to see if something may be captured, but also check to avoid duplicates. Dupes are unneeded.
+                    if (!(dangerSpots.Contains(futureSpot)))
+                    {
+                        Debug.Log("Added danger spot from knight at " + futureSpot);
+                        dangerSpots.Add(futureSpot);
+                    }
+                }
+            }
+
+        }
+        return dangerSpots;
+    }
+
+    //function to handle both updating the capture list, and updating the dict.
+    private void AddToCaptureList(Vector2Int dz, Piece piece)
+    {
+        if(!(potentialCaptures.Contains(dz)))
+        potentialCaptures.Add(dz);
+
+        //check to make sure that the piece isn't in the dict already (i.e. in danger of capture by another piece)
+        //and decide on which logic needs to be used to add to the list of vulnerabilities.
+        if (capturePairs.ContainsKey(dz))
+        {
+            capturePairs[dz].Add(piece.occupiedSquare);
+        }
+        else
+        {
+            List<Vector2Int> starterList = new List<Vector2Int>
+                            {
+                                piece.occupiedSquare
+                            };
+            capturePairs.Add(dz, starterList);
+        }
+    }
+
+    //written to generate a list of pieces in danger, and a dictionary which links the list of pieces to 
+    //each piece that could potentially capture the pieces in danger.
+    //designed with defense in mind, but could probably be useful to offense applications as well.
+
+    //INPUT: a list of pieces from the attacking team (example: controller.whitePlayer.ActivePieces)
+    //OUTPUT: modifies List<Vector2Int> potentialCaptures and dictionary capturePairs to contain all potential offensive moves for the attacking team.
+    private void PotentialCaptureFinder(List<Piece> pieces)
+    {
+        //ensure these two are emptied before each run of PotentialCaptureFinder.
+        potentialCaptures.Clear();
+        capturePairs.Clear();
+        foreach (Piece corpPiece in pieces.ToList())
+        {
+            //Debug.Log("Reached inside of enemypieces");
+            //Debug.Log("Number of enemy pieces: " + enemyPieces.Count);
+            //Debug.Log("Available moves: " + corpPiece.AvailableMoves.Count);
+            Vector3 piecePosition = board.GetPositionFromCoords(corpPiece.occupiedSquare);
+            Debug.Log("Checking potential for movements from enemy piece " + corpPiece.pieceType + " at coordinate " + corpPiece.occupiedSquare);
+
+            if (corpPiece.AvailableMoves.Count > 0)
+            {
+                //Handle finding the true area a knight can capture. Knight's AvailableMoves doesn't give an honest representation.
+                if (corpPiece.pieceType.ToString().Equals("Knight"))
+                {
+                    Debug.Log("Found special rule for " + corpPiece.pieceType);
+                    List<Vector2Int> knightDangerZone = KnightHighDangerZone(corpPiece);
+                    Debug.Log("Now listing places knight can capture");
+                    foreach (Vector2Int dz in knightDangerZone.ToList())
+                    {
+                        Debug.Log(dz);
+                        AddToCaptureList(dz, corpPiece);
+
+                    }
+                    Debug.Log("End of List.");
+                }
+
+
+                foreach (Vector2Int move in corpPiece.AvailableMoves.ToList())
+                {
+                    //see if we can detect that a "take" is available.
+                    //Debug.Log("Reached inside of availablemoves");
+                    Debug.Log(move);
+                    Piece piece = board.GetPieceOnSquare(move);
+                    if (piece != null && !corpPiece.IsFromSameTeam(piece))
+                    {
+                        Debug.Log("Found cap");
+                        AddToCaptureList(move, corpPiece);
+                    }
+
+                }
+            }
+        }
+    }
+
     public void AI_TakeTurn() 
     {
         StartCoroutine(AI_TakeTurn_Coroutine());
