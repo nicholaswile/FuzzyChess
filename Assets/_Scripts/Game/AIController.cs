0b07362fce6099630cc530439c3f25f663b393da
DesktopCoder1407 <dcoder1407@gmail.com>
2022-04-14 01:09:41 -0400

    Generalize AI for attacking all pieces
    Includes special movements for Knights

--- Assets/_Scripts/Game/AIController.cs
+++ Assets/_Scripts/Game/AIController.cs
@@ -19,22 +19,21 @@ public class AIController : MonoBehaviour
         {PieceType.Pawn, new Dictionary<PieceType, int>() {{PieceType.King, 6}, {PieceType.Queen, 6}, {PieceType.Knight, 6}, {PieceType.Bishop, 5}, {PieceType.Rook, 6}, {PieceType.Pawn, 4}}}
     };
     private Dictionary<PieceType, int> captureWorth = new Dictionary<PieceType, int>() {
-        {PieceType.King, 5000},
-        {PieceType.Queen, 70},
-        {PieceType.Knight, 65},
-        {PieceType.Bishop, 80},
-        {PieceType.Rook, 25},
-        {PieceType.Pawn, 10}
-    };
-    private Dictionary<PieceType, int> defenseWorth = new Dictionary<PieceType, int>() {
-        {PieceType.King, 4000},
-        {PieceType.Queen, 75},
-        {PieceType.Knight, 55},
+        {PieceType.King, 101},
+        {PieceType.Queen, 40},
+        {PieceType.Knight, 60},
         {PieceType.Bishop, 70},
-        {PieceType.Rook, 10},
+        {PieceType.Rook, 50},
         {PieceType.Pawn, 5}
     };
-    private float threshold = 10.0f; //A move must be greater than or equal to this threshold to be chosen
+    private Dictionary<PieceType, int> defenseWorth = new Dictionary<PieceType, int>() {
+        {PieceType.King, 100},
+        {PieceType.Queen, 40},
+        {PieceType.Knight, 60},
+        {PieceType.Bishop, 70},
+        {PieceType.Rook, 50},
+        {PieceType.Pawn, 5}
+    };
 
     //IsAIAttacking | AI Piece | Opponent Piece | Roll Requirement | Value of the Piece Under Attack | Cost of Move
     private List<ArrayList> moveList = new List<ArrayList>(); 
@@ -52,6 +51,7 @@ public class AIController : MonoBehaviour
             updateMoveList(aiPieces, enemyPieces);
             moveList.Sort(sortMoveList);
 
+            //DEBUG: Output number of moves avaliable, what's attacking, what's being attacked, and all values related to movement.
             Debug.Log("There are " + moveList.Count + " capturing moves that can be made.");
             foreach (ArrayList a in moveList)
             {
@@ -61,51 +61,59 @@ public class AIController : MonoBehaviour
                     Debug.Log("AI " + ((Piece)a[1]).pieceType + " is being attacked by Player " + ((Piece)a[2]).pieceType + ". A roll of " + a[3] + " will capture. The value of this piece is " + a[4] + ". Cost: " + a[5]);
             }
 
-            if (moveList.Count > 0 && (bool)moveList.ElementAt(0)[0] == true && ((Piece)moveList.ElementAt(0)[2]).pieceType == PieceType.King)
-            {
-                Piece attackingPiece = ((Piece)moveList.ElementAt(0)[1]);
-                Vector3 piecePosition = board.GetPositionFromCoords(attackingPiece.occupiedSquare);
-                Vector3 movePosition = board.GetPositionFromCoords(((Piece)moveList.ElementAt(0)[2]).occupiedSquare);
+            int i = 0;
+            while (i < moveList.Count && (bool)moveList.ElementAt(i)[0] == false)
+                i++;
 
-                //Select square with piece that can attack opponent King
-                yield return new WaitForSeconds(1);
+            if (moveList.Count > 0 && i < moveList.Count)
+            {
+
+                Piece attackingPiece = ((Piece)moveList.ElementAt(i)[1]);
+                Vector3 piecePosition = board.GetPositionFromCoords(attackingPiece.occupiedSquare);
+                Vector3 movePosition = board.GetPositionFromCoords(((Piece)moveList.ElementAt(i)[2]).occupiedSquare);
+
+                //Select square with piece that can attack opponent
+                yield return new WaitForSeconds(2.5f);
                 board.OnSquareSelected(piecePosition);
 
-                if(attackingPiece.pieceType == PieceType.Knight)
+                if (attackingPiece.pieceType != PieceType.Knight)
                 {
-                    Vector2Int preAttackSquare = new Vector2Int(-1, -1);
+                    //Attack (Begins Roll)
+                    yield return new WaitForSeconds(1.25f);
+                    board.OnSquareSelected(movePosition);
+                    SFXController.PlaySoundMovement();
+                }
+                else
+                {
+                    //Knight Determine Movement Location before Attack
+                    Vector2Int preAttackSquare = attackingPiece.occupiedSquare;
                     foreach (Vector2Int move in attackingPiece.AvailableMoves)
                     {
                         if (board.GetPieceOnSquare(move) == null)
                         {
                             foreach (Vector2Int doubleMove in attackingPiece.GetAdjacentEnemySquares(move))
                             {
-                                if (doubleMove == ((Piece)moveList.ElementAt(0)[2]).occupiedSquare)
+                                if (doubleMove == ((Piece)moveList.ElementAt(i)[2]).occupiedSquare)
                                 {
                                     preAttackSquare = move;
                                     break;
                                 }
                             }
-                            if (preAttackSquare != new Vector2Int(-1,-1))
+                            if (preAttackSquare != attackingPiece.occupiedSquare)
                                 break;
                         }
                     }
+                
+                    //Knight take Pre-Attack Jump if Jump exists.
+                    if(preAttackSquare != attackingPiece.occupiedSquare)
+                    {
+                        yield return new WaitForSeconds(1.25f);
+                        board.OnSquareSelected(board.GetPositionFromCoords(preAttackSquare));
+                        SFXController.PlaySoundMovement();
+                    }
 
-                    //Select square next to opponent King (Begins knight pre-attack)
-                    yield return new WaitForSeconds(1);
-                    board.OnSquareSelected(board.GetPositionFromCoords(preAttackSquare));
-                    SFXController.PlaySoundMovement();
-
-                    //Select square with opponent King (Begins attack roll)
-                    yield return new WaitForSeconds(1);
-                    board.OnSquareSelected(movePosition);
-                    SFXController.PlaySoundMovement();
-
-                }
-                else
-                {
-                    //Select square with opponent King (Begins attack roll)
-                    yield return new WaitForSeconds(1);
+                    //Knight Attack (Begins Roll with +1)
+                    yield return new WaitForSeconds(1.25f);
                     board.OnSquareSelected(movePosition);
                     SFXController.PlaySoundMovement();
                 }
@@ -115,31 +123,16 @@ public class AIController : MonoBehaviour
                 foreach (Piece corpPiece in activeCorpPieces.ToList())
                 {
                     Vector3 piecePosition = board.GetPositionFromCoords(corpPiece.occupiedSquare);
-
                     if (board.isSelectable(corpPiece) && corpPiece.AvailableMoves.Count > 0)
                     {
-
-                        //Debug.Log(corpPiece.AvailableMoves.Count);
-
-                        yield return new WaitForSeconds(1);
+                        yield return new WaitForSeconds(2.5f);
                         board.OnSquareSelected(piecePosition);
 
                         foreach (Vector2Int move in corpPiece.AvailableMoves.ToList())
                         {
-
-                            //see if we can detect that a "take" is available.
-                            //Debug.Log(move);
-                            //Piece piece = board.GetPieceOnSquare(move);
-                            //if (piece != null && !corpPiece.IsFromSameTeam(piece)) Debug.Log("Found cap");
-                        }
-
-                        foreach (Vector2Int move in corpPiece.AvailableMoves.ToList())
-                        {
-                            //Debug.Log(move);
-
                             Vector3 movePosition = board.GetPositionFromCoords(move);
 
-                            yield return new WaitForSeconds(1);
+                            yield return new WaitForSeconds(1.25f);
                             board.OnSquareSelected(movePosition);
                             SFXController.PlaySoundMovement();
 
@@ -202,7 +195,7 @@ public class AIController : MonoBehaviour
                     {
                         //Add all additional knight moves.
                         Piece enemyPiece = board.GetPieceOnSquare(move);
-                        moveList.Add(new ArrayList() { true, aiPiece, enemyPiece, getMinRoll(aiPiece, enemyPiece) - 1, captureWorth[enemyPiece.pieceType], getMoveCost(getMinRoll(aiPiece, enemyPiece), captureWorth[enemyPiece.pieceType]) });
+                        moveList.Add(new ArrayList() { true, aiPiece, enemyPiece, getMinRoll(aiPiece, enemyPiece) - 1, captureWorth[enemyPiece.pieceType], getMoveCost(getMinRoll(aiPiece, enemyPiece) - 1, captureWorth[enemyPiece.pieceType]) });
                     }
                 }
 
@@ -229,7 +222,7 @@ public class AIController : MonoBehaviour
                     {
                         //Add all additional knight moves.
                         Piece aiPiece = board.GetPieceOnSquare(move);
-                        moveList.Add(new ArrayList() { false, aiPiece, enemyPiece, getMinRoll(enemyPiece, aiPiece) - 1, defenseWorth[aiPiece.pieceType], getMoveCost(getMinRoll(enemyPiece, aiPiece), defenseWorth[aiPiece.pieceType]) });
+                        moveList.Add(new ArrayList() { false, aiPiece, enemyPiece, getMinRoll(enemyPiece, aiPiece) - 1, defenseWorth[aiPiece.pieceType], getMoveCost(getMinRoll(enemyPiece, aiPiece) - 1, defenseWorth[aiPiece.pieceType]) });
                     }
                 }
 
@@ -244,21 +237,17 @@ public class AIController : MonoBehaviour
             }
         }
 
-        moveList.RemoveAll(outsideThreshold);
         moveList.RemoveAll(corpMoved);
     }
 
-    private int getMinRoll(Piece attacking, Piece defending) {
+    private int getMinRoll(Piece attacking, Piece defending)
+    {
         return captureTable[attacking.pieceType][defending.pieceType];
     }
 
-    private float getMoveCost(int minRoll, int pieceValue) {
-        return ((float)1/minRoll * 1.5f) * pieceValue;
-    }
-
-    private bool outsideThreshold(ArrayList move)
+    private float getMoveCost(int minRoll, int pieceValue)
     {
-        return (float)move[5] < threshold;
+        return ((float)1/minRoll) * pieceValue;
     }
 
     private int sortMoveList(ArrayList x, ArrayList y)
